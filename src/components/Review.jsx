83ebdabf701848a324ecd0e243d424e2c010120d
yasirmaj <yasir.majid@hotmail.co.uk>
2022-12-06 11:20:37 +0000

    6 View a list of comments associated with a review

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/dist";
 import { getReviewsById } from "../utils/api";
+import Comments from "./Comments";
 
 export default function Review() {
   const [review, setReview] = useState({});
@@ -23,23 +24,28 @@ export default function Review() {
       {isLoading ? (
         <h3 id="loading">Loading...</h3>
       ) : (
-        <section className="review">
-          <h3>{review.title}</h3>
-          <p>Designer: {review.designer}</p>
-          <p>Category: {review.category}</p>
-          <img
-            src={review.review_img_url}
-            alt={review.title}
-            width="200"
-            height="200"
-          />
-          <br />
-          <p>Author: {review.owner}</p>
-          <p>{review.review_body}</p>
-          <p>Date Created: {review.created_at}</p>
-          <span>Votes: {review.votes}</span>
-          <span>Comments: {review.comment_count}</span>
-        </section>
+        <div>
+          <section className="review">
+            <h3>{review.title}</h3>
+            <p>Designer: {review.designer}</p>
+            <p>Category: {review.category}</p>
+            <img
+              src={review.review_img_url}
+              alt={review.title}
+              width="200"
+              height="200"
+            />
+            <br />
+            <p>Author: {review.owner}</p>
+            <p>{review.review_body}</p>
+            <p>Date Created: {review.created_at}</p>
+            <span>Votes: {review.votes}</span>
+            <span>Comments: {review.comment_count}</span>
+          </section>
+          <section className="comments-list">
+            <Comments />
+          </section>
+        </div>
       )}
     </main>
   );
